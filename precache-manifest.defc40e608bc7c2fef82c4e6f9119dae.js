45cf76bd134986141103068c7a794ea8afdda1f0
Nikolay Elesin <elesinn@ya.ru>
2020-04-20 13:43:14 +0300

    Updates

---
+++ precache-manifest.defc40e608bc7c2fef82c4e6f9119dae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afeeb82d00959fe966309d4ee238902",
+    "url": "/poke_list/index.html"
+  },
+  {
+    "revision": "2706ba79db5b1d04b540",
+    "url": "/poke_list/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "5b909c387b3395de137e",
+    "url": "/poke_list/static/css/main.ff2cc17e.chunk.css"
+  },
+  {
+    "revision": "2706ba79db5b1d04b540",
+    "url": "/poke_list/static/js/2.5135fee6.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/poke_list/static/js/2.5135fee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b909c387b3395de137e",
+    "url": "/poke_list/static/js/main.fd6103dd.chunk.js"
+  },
+  {
+    "revision": "1d4842c568744d55cc59",
+    "url": "/poke_list/static/js/runtime-main.04983537.js"
+  },
+  {
+    "revision": "498b37b6fdb79609e18d295f1a5759cd",
+    "url": "/poke_list/static/media/default.498b37b6.jpg"
+  }
+]);
